945b8bb8a9161ce4445d6504ad281ef77a615062
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2022-12-09 14:35:44 +0100

    it couldn't find the collection "destinations", but i found the collectiongroup. now the data displays in the recyclerview.

--- app/src/main/java/com/example/wonders/DestinationRecycleAdapter.kt
+++ app/src/main/java/com/example/wonders/DestinationRecycleAdapter.kt
@@ -4,27 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
 
 
-class DestinationRecycleAdapter(val context: Context, private val destinationList: MutableList<Destination>) : RecyclerView.Adapter<DestinationRecycleAdapter.ViewHolder>() {
+class DestinationRecycleAdapter(val context: Context, val destinationArray: List<Destination>) : RecyclerView.Adapter<DestinationRecycleAdapter.ViewHolder>() {
 
-    val db = FirebaseFirestore.getInstance()
-    val collectionRef = db.collection("destinations")
-
-    val listener = collectionRef.addSnapshotListener{ snapshot, exception ->
-        if(exception != null){
-            return@addSnapshotListener
-        }
-        destinationList.clear()
-        for (document in snapshot!!) {
-            val destination = document.toObject<Destination>(Destination::class.java)
-            destinationList.add(destination)
-        }
-        notifyDataSetChanged()
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(context).inflate(R.layout.destination_list,parent,false)
@@ -32,27 +18,23 @@ class DestinationRecycleAdapter(val context: Context, private val destinationLis
         return ViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val destination = destinationList[position]
 
-        //holder.picture.setImageDrawable(holder.picture.context.getDrawable(destination.picture))
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val destination = destinationArray[position]
+
+        //holder.picture.setImageDrawable(holder.picture.context.getDrawable(Destination.picture))
         holder.countryView.text = destination.country
         holder.placeView.text = destination.place
         holder.infoView.text = destination.info
     }
 
     override fun getItemCount(): Int {
-        return destinationList.size
-    }
-
-    override fun onViewRecycled(holder: ViewHolder) {
-        super.onViewRecycled(holder)
-        listener.remove()
+        return destinationArray.size
     }
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
 
-        //val picture : ImageView = itemView.findViewById(R.id.imageView)
+        //val picture : ImageView = itemView.findViewById(R.id.imageOfPlace)
         val countryView : TextView = itemView.findViewById(R.id.countryTextView)
         val placeView : TextView = itemView.findViewById(R.id.placeTextView)
         val infoView : TextView = itemView.findViewById(R.id.infoTextView)
--- app/src/main/java/com/example/wonders/ListActivity.kt
+++ app/src/main/java/com/example/wonders/ListActivity.kt
@@ -3,45 +3,30 @@ package com.example.wonders
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.firestore.*
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class ListActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
-    val db = FirebaseFirestore.getInstance()
-    val collectionRef = db.collection("destinations")
-
-    private lateinit var listener : ListenerRegistration
-
+    lateinit var adapter : DestinationRecycleAdapter
+    val destinationArray = mutableListOf<Destination>()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
+
         recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        listener = collectionRef.addSnapshotListener { snapshot, exception ->
-            if (exception != null) {
-                return@addSnapshotListener
-            }
-
-            val destinationArray = mutableListOf<Destination>()
-            for (document in snapshot!!) {
-                val destination = document.toObject<Destination>(Destination::class.java)
-                destinationArray.add(destination)
-            }
-            val adapter = DestinationRecycleAdapter(this, destinationArray)
-            recyclerView.adapter = adapter
-        }
+        getData()
 
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
@@ -49,19 +34,43 @@ class ListActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun getData(){
+
+        val db = Firebase.firestore
+
+        val collectionRef = db.collectionGroup("destinations")
+        collectionRef.addSnapshotListener { snapshot, exception ->
+            if(exception != null){
+                return@addSnapshotListener
+            }
+
+            snapshot?.forEach { document ->
+                //destinationArray.clear()
+                val destination = document.toObject<Destination>(Destination::class.java)
+                destinationArray.add(destination)
+            }
+            // Check if the destinationArray list is not empty
+            if (destinationArray.isEmpty()) {
+                // Log a message if the destinationArray list is empty
+                Log.d("DestinationArray", "The destinationArray list is empty")
+            } else {
+                // Log the size of the destinationArray list
+                Log.d("DestinationArray", "The destinationArray list has ${destinationArray.size} elements")
+            }
+
+            recyclerView.layoutManager = LinearLayoutManager(this)
+
+            adapter = DestinationRecycleAdapter(this, destinationArray)
+            recyclerView.adapter = adapter
+
+            adapter.notifyDataSetChanged()
+        }
+
+    }
+
 }
 
 
-/*
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
 
-        destinationArray = arrayListOf()
-
-
-        adapter = DestinationRecycleAdapter(destinationArray)
-        recyclerView.adapter = adapter
-
- */
 
